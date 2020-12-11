44f3f43b4561f9a865e8433c23575baf6d6f39ac
Tianyi <ShiTianyi2001@outlook.com>
2020-12-11 10:51:31 +0000

    lca

--- src/algo/graph/tree.rs
+++ src/algo/graph/tree.rs
@@ -2,6 +2,7 @@ pub mod center;
 pub mod height;
 pub mod height1;
 pub mod isomorphism;
+pub mod lca;
 pub mod rooting;
 pub mod rooting1;
 pub mod sum;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(slice_ptr_len)]
 pub mod algo;
 pub mod data_structures;
 pub mod problems;
