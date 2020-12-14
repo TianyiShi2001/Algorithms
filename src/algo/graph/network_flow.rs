133771bcd5fdae3c15f23e53ce6238e6295d6672
Tianyi <ShiTianyi2001@outlook.com>
2020-12-14 01:03:06 +0000

    max flow

--- src/algo/graph/network_flow.rs
+++ src/algo/graph/network_flow.rs
@@ -1,27 +1,33 @@
 pub mod max_flow;
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Edge {
     pub to: usize,
     pub flow: i32,
     pub capacity: i32,
+    pub residual: Weak<RefCell<Edge>>,
 }
 impl Edge {
-    pub fn new(to: usize, capacity: i32) -> Self {
-        Self {
-            to,
-            capacity,
-            flow: 0,
-        }
+    pub fn is_residual(&self) -> bool {
+        self.capacity == 0
+    }
+    pub fn reamaining_capacity(&self) -> i32 {
+        self.capacity - self.flow
+    }
+    pub fn augment(&mut self, bottleneck: i32) {
+        self.flow += bottleneck;
+        self.residual.upgrade().unwrap().borrow_mut().flow -= bottleneck;
     }
 }
 
 #[derive(Debug)]
-pub struct WeightedAdjacencyList {
-    edges: Vec<Vec<Edge>>,
+pub struct NetworkFlowAdjacencyList {
+    edges: Vec<Vec<Rc<RefCell<Edge>>>>,
 }
 
-impl WeightedAdjacencyList {
+impl NetworkFlowAdjacencyList {
     /// Initialize an empty adjacency list that can hold up to n nodes.
     pub fn with_size(n: usize) -> Self {
         Self {
@@ -31,39 +37,43 @@ impl WeightedAdjacencyList {
     pub fn is_empty(&self) -> bool {
         self.edges.is_empty()
     }
-    /// Add a directed edge from node `u` to node `v` with cost `cost`.
-    pub fn add_directed_edge(&mut self, u: usize, v: usize, cost: i32) {
-        self.edges[u].push(Edge::new(v, cost))
+    pub fn add_edge(&mut self, from: usize, to: usize, capacity: i32) {
+        let e1 = Rc::new(RefCell::new(Edge {
+            to,
+            capacity,
+            flow: 0,
+            residual: Weak::default(),
+        }));
+        let e2 = Rc::new(RefCell::new(Edge {
+            to: from,
+            capacity: 0,
+            flow: 0,
+            residual: Weak::default(),
+        }));
+        e1.borrow_mut().residual = Rc::downgrade(&e2);
+        e2.borrow_mut().residual = Rc::downgrade(&e1);
+
+        self.edges[from].push(e1);
+        self.edges[to].push(e2);
     }
-    /// Add an undirected edge between nodes `u` and `v`.
-    pub fn add_undirected_edge(&mut self, u: usize, v: usize, cost: i32) {
-        self.add_directed_edge(u, v, cost);
-        self.add_directed_edge(v, u, cost);
-    }
-    pub fn new_directed(size: usize, edges: &[(usize, usize, i32)]) -> Self {
+    pub fn from_edges(size: usize, edges: &[(usize, usize, i32)]) -> Self {
         let mut graph = Self::with_size(size);
         for &(a, b, c) in edges.iter() {
-            graph.add_directed_edge(a, b, c);
-        }
-        graph
-    }
-    pub fn new_undirected(size: usize, edges: &[(usize, usize, i32)]) -> Self {
-        let mut graph = Self::with_size(size);
-        for &(a, b, c) in edges.iter() {
-            graph.add_undirected_edge(a, b, c);
+            graph.add_edge(a, b, c);
         }
         graph
     }
     pub fn edges(&self) -> impl Iterator<Item = (usize, usize, i32, i32)> + '_ {
-        self.edges
-            .iter()
-            .enumerate()
-            .flat_map(|(a, edges)| edges.iter().map(move |b| (a, b.to, b.flow, b.capacity)))
+        self.edges.iter().enumerate().flat_map(|(a, edges)| {
+            edges
+                .iter()
+                .map(move |b| (a, b.borrow().to, b.borrow().flow, b.borrow().capacity))
+        })
     }
     pub fn edges_count(&self) -> usize {
         self.edges().count()
     }
-    pub fn vertices(&self) -> impl Iterator<Item = (usize, &Vec<Edge>)> {
+    pub fn vertices(&self) -> impl Iterator<Item = (usize, &Vec<Rc<RefCell<Edge>>>)> {
         self.edges.iter().enumerate()
     }
     pub fn vertices_count(&self) -> usize {
@@ -71,9 +81,15 @@ impl WeightedAdjacencyList {
     }
 }
 
-impl std::ops::Index<usize> for WeightedAdjacencyList {
-    type Output = Vec<Edge>;
+impl std::ops::Index<usize> for NetworkFlowAdjacencyList {
+    type Output = Vec<Rc<RefCell<Edge>>>;
     fn index(&self, index: usize) -> &Self::Output {
         &self.edges[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for NetworkFlowAdjacencyList {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.edges[index]
     }
 }
--- src/algo/graph/network_flow/max_flow.rs
+++ src/algo/graph/network_flow/max_flow.rs
@@ -8,5 +8,138 @@
 //!
 //! - [W. Fiset's video 1](https://www.youtube.com/watch?v=LdOnanfc5TM&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=33)
 //! - [W. Fiset's video 2](https://www.youtube.com/watch?v=Xu8jjJnwvxE&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=34)
-//! - [W. Fiset's video 3](https://www.youtube.com/watch?v=CI5Fvk-dGVs&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=32)
 //! - [Wikipedia](https://www.wikiwand.com/en/Ford%E2%80%93Fulkerson_algorithm)
+
+use super::{Edge, NetworkFlowAdjacencyList};
+
+const INF: i32 = i32::MAX / 2;
+
+struct FordFulkersonSolver<'a> {
+    g: &'a mut NetworkFlowAdjacencyList,
+    s: usize,
+    t: usize,
+    visited: Vec<u32>,
+    visited_token: u32,
+}
+
+impl<'a> FordFulkersonSolver<'a> {
+    pub fn init(g: &'a mut NetworkFlowAdjacencyList, source: usize, sink: usize) -> Self {
+        let n = g.vertices_count();
+        Self {
+            g,
+            s: source,
+            t: sink,
+            visited: vec![0; n],
+            visited_token: 1,
+        }
+    }
+    pub fn solve(&mut self) -> i32 {
+        let mut flow = 0;
+        let mut f = -1;
+        while f != 0 {
+            f = self.dfs(self.s, INF);
+            flow += f;
+            self.visited_token += 1;
+        }
+        flow
+    }
+    pub fn dfs(&mut self, node: usize, flow: i32) -> i32 {
+        // at sink node, return augmented path flow
+        if node == self.t {
+            return flow;
+        }
+        self.visited[node] = self.visited_token;
+        for edge in
+            unsafe { &mut *(&mut self.g[node] as *mut Vec<std::rc::Rc<std::cell::RefCell<Edge>>>) }
+        {
+            let rcap = edge.borrow().reamaining_capacity();
+            if self.visited[edge.borrow().to] != self.visited_token && rcap > 0 {
+                let bottleneck = self.dfs(edge.borrow().to, std::cmp::min(flow, rcap));
+                // if we made it from s --> t (a.k.a bottleneck > 0) then augment flow with the bottleneck value
+                if bottleneck > 0 {
+                    edge.borrow_mut().augment(bottleneck);
+                    return bottleneck;
+                }
+            }
+        }
+        0
+    }
+}
+
+impl NetworkFlowAdjacencyList {
+    pub fn ford_fulkerson(&mut self, source: usize, sink: usize) -> i32 {
+        let mut s = FordFulkersonSolver::init(self, source, sink);
+        s.solve()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_max_flow(n: usize, edges: &[(usize, usize, i32)], expected_max_flow: i32) {
+        let mut graph = NetworkFlowAdjacencyList::from_edges(n, edges);
+        let max_flow = graph.ford_fulkerson(n - 1, n - 2);
+        assert_eq!(max_flow, expected_max_flow);
+    }
+
+    #[test]
+    fn test_small_graph() {
+        test_max_flow(
+            6,
+            &[
+                // Source edges
+                (5, 0, 10),
+                (5, 1, 10),
+                // Sink edges
+                (2, 4, 10),
+                (3, 4, 10),
+                // Middle edges
+                (0, 1, 2),
+                (0, 2, 4),
+                (0, 3, 8),
+                (1, 3, 9),
+                (3, 2, 6),
+            ],
+            19,
+        );
+    }
+
+    #[test]
+    fn test_disconnected() {
+        test_max_flow(4, &[(3, 0, 9), (1, 2, 9)], 0);
+    }
+
+    #[test]
+    fn test_medium_graph() {
+        test_max_flow(
+            12,
+            &[
+                // from source
+                (11, 0, 5),
+                (11, 1, 20),
+                (11, 2, 10),
+                // to sink
+                (7, 10, 7),
+                (8, 10, 15),
+                (9, 10, 60),
+                // middle
+                (0, 1, 3),
+                (0, 5, 4),
+                (1, 4, 14),
+                (1, 5, 14),
+                (2, 1, 5),
+                (2, 3, 4),
+                (3, 4, 3),
+                (3, 9, 11),
+                (4, 6, 4),
+                (4, 8, 22),
+                (5, 6, 8),
+                (5, 7, 3),
+                (6, 7, 12),
+                (7, 8, 9),
+                (8, 9, 11),
+            ],
+            29,
+        );
+    }
+}
