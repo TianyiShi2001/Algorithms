2aaa1eb15bfe8d922a18331a18ef15f580498cf2
Tianyi <ShiTianyi2001@outlook.com>
2020-12-17 11:35:51 +0000

    sparse table

--- src/data_structures/sparse_table.rs
+++ src/data_structures/sparse_table.rs
@@ -1,4 +1,10 @@
-use std::fmt::Debug;
+//! Implementation of a sparse table which is a data structure that can very quickly query a range on
+//! a static array in $O(1)$ for overlap-friendly functions (idempotent functions) using $O(n*logn)$ memory.
+//! For functions that are only associative, the query is done in $O(log(n))$.
+//!
+//! A function $f$ is associative if $f(a, f(b, c)) = f(f(a, b), c)$. Examples include scalar and matrix
+//! addition and multiplication.
+//! A function is overlap-freindly if $f(f(a, b), f(b, c)) = f(f(a, b), c)$. Examples include min, max and gcd.
 
 pub struct SparseTable<T, F>
 where
@@ -6,13 +12,15 @@ where
 {
     // The sparse table values.
     values: Vec<Vec<Option<T>>>,
-    // Index Table associated with the values in the sparse table.
+    // Pre-computed array of log2 values
     log2: Vec<usize>,
+    // The function to be applied
     f: F,
+    overlap_friendly: bool,
 }
 
-impl<T: Clone + Debug, F: Fn(T, T) -> T> SparseTable<T, F> {
-    pub fn new(arr: &[T], f: F) -> Self {
+impl<T: Clone, F: Fn(T, T) -> T> SparseTable<T, F> {
+    pub fn new(arr: &[T], f: F, overlap_friendly: bool) -> Self {
         let n = arr.len();
         let log2 = Self::build_log2(n);
         let m = log2[n];
@@ -28,7 +36,12 @@ impl<T: Clone + Debug, F: Fn(T, T) -> T> SparseTable<T, F> {
                 values[i][j] = Some(f(left_interval.unwrap(), right_interval.unwrap()));
             }
         }
-        Self { values, log2, f }
+        Self {
+            values,
+            log2,
+            f,
+            overlap_friendly,
+        }
     }
     fn build_log2(n: usize) -> Vec<usize> {
         let mut log2 = vec![0usize; n + 1];
@@ -37,24 +50,25 @@ impl<T: Clone + Debug, F: Fn(T, T) -> T> SparseTable<T, F> {
         }
         log2
     }
-    pub fn query(&self, l: usize, r: usize) -> T {
-        let len = r - l + 1;
-        let i = self.log2[len];
-        let left_interval = self.values[i][l].clone();
-        let right_interval = self.values[i][1 + r - (1 << i)].clone();
-        (self.f)(left_interval.unwrap(), right_interval.unwrap())
-    }
-    pub fn query_acc(&self, mut l: usize, r: usize) -> T {
-        let mut p = self.log2[1 + r - l];
-        let mut acc = self.values[p][l].clone().unwrap();
-        l += 1 << p;
-        while l <= r {
-            p = self.log2[1 + r - l];
-            acc = (self.f)(acc, self.values[p][l].clone().unwrap());
+    pub fn query(&self, mut l: usize, r: usize) -> T {
+        if self.overlap_friendly {
+            let len = r - l + 1;
+            let i = self.log2[len];
+            let left_interval = self.values[i][l].clone();
+            let right_interval = self.values[i][1 + r - (1 << i)].clone();
+            (self.f)(left_interval.unwrap(), right_interval.unwrap())
+        } else {
+            let mut p = self.log2[1 + r - l];
+            let mut acc = self.values[p][l].clone().unwrap();
             l += 1 << p;
-        }
+            while l <= r {
+                p = self.log2[1 + r - l];
+                acc = (self.f)(acc, self.values[p][l].clone().unwrap());
+                l += 1 << p;
+            }
 
-        acc
+            acc
+        }
     }
 }
 
@@ -66,27 +80,23 @@ mod tests {
 
     const SAMPLE_SIZE: usize = 10;
     lazy_static! {
-        static ref TEST_DATA: Vec<i32> = {
+        static ref TEST_DATA: Vec<u32> = {
             let mut rng = thread_rng();
-            (0..SAMPLE_SIZE).map(|_| rng.gen_range(-10, 10)).collect()
+            (0..SAMPLE_SIZE).map(|_| rng.gen_range(0, 10)).collect()
         };
     }
 
     fn validate<F>(f: F, overlap_ok: bool)
     where
-        F: Fn(i32, i32) -> i32,
+        F: Fn(u32, u32) -> u32,
     {
-        let sparse_table = SparseTable::new(&TEST_DATA, f);
+        let sparse_table = SparseTable::new(&TEST_DATA, f, overlap_ok);
         for i in 0..SAMPLE_SIZE - 1 {
             for j in i..SAMPLE_SIZE {
                 let expected = TEST_DATA[i + 1..=j]
                     .iter()
                     .fold(TEST_DATA[i], |acc, curr| (sparse_table.f)(acc, *curr));
-                let quried = if overlap_ok {
-                    sparse_table.query(i, j)
-                } else {
-                    sparse_table.query_acc(i, j)
-                };
+                let quried = sparse_table.query(i, j);
                 assert_eq!(expected, quried);
             }
         }
@@ -99,8 +109,65 @@ mod tests {
     }
 
     #[test]
-    fn test_sparse_table_sum_mul() {
+    fn test_sparse_table_add_mul() {
         validate(|a, b| a + b, false);
         validate(|a, b| a * b, false);
+    }
+
+    #[test]
+    fn test_gcd() {
+        use crate::algo::math::gcd::GcdUnsigned;
+        validate(|a, b| a.gcd(b), true);
+    }
+
+    #[test]
+    fn test_sparse_table_matrix_add_mul() {
+        let mut rng = thread_rng();
+        let data: Vec<Matrix2x2> = (0..SAMPLE_SIZE * 4)
+            .map(|_| rng.gen_range(-10, 10))
+            .collect::<Vec<_>>()
+            .chunks_exact(4)
+            .map(|x| [[x[0], x[1]], [x[2], x[3]]])
+            .collect();
+
+        let sparse_table_add = SparseTable::new(&data, matrix_addition_2x2, false);
+        let sparse_table_mul = SparseTable::new(&data, matrix_multiplication_2x2, false);
+        for i in 0..SAMPLE_SIZE - 1 {
+            for j in i..SAMPLE_SIZE {
+                let (expected_sum, expected_product) =
+                    data[i + 1..=j]
+                        .iter()
+                        .fold((data[i], data[i]), |acc, curr| {
+                            (
+                                matrix_addition_2x2(acc.0, *curr),
+                                matrix_multiplication_2x2(acc.1, *curr),
+                            )
+                        });
+                let quried_sum = sparse_table_add.query(i, j);
+                let quried_product = sparse_table_mul.query(i, j);
+                assert_eq!(expected_sum, quried_sum);
+                assert_eq!(expected_product, quried_product);
+            }
+        }
+    }
+
+    type Matrix2x2 = [[i32; 2]; 2];
+    fn matrix_addition_2x2(a: Matrix2x2, b: Matrix2x2) -> Matrix2x2 {
+        [
+            [a[0][0] + b[0][0], a[0][1] + b[0][1]],
+            [a[1][0] + b[1][0], a[1][1] + b[1][1]],
+        ]
+    }
+    fn matrix_multiplication_2x2(a: Matrix2x2, b: Matrix2x2) -> Matrix2x2 {
+        [
+            [
+                a[0][0] * b[0][0] + a[0][1] * b[1][0],
+                a[0][0] * b[0][1] + a[0][1] * b[1][1],
+            ],
+            [
+                a[1][0] * b[0][0] + a[1][1] * b[1][0],
+                a[1][0] * b[0][1] + a[1][1] * b[1][1],
+            ],
+        ]
     }
 }
