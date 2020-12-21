2e83f079bfeec7b0f9a0a6296e7685864b169d81
Tianyi <ShiTianyi2001@outlook.com>
2020-12-21 22:17:05 +0000

    remove leetcode

--- leetcode-boost/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! This crate aims to provide data structures and algorithms that can be easily reused (copy-pasted)
-//! in competitive programming where it's legitimate to do so.
-
-// WIP
-
-pub mod graph;
--- leetcode/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod _utils;
-pub mod q1129;
-pub mod q1267;
-pub mod q1387;
-pub mod q1557;
-pub mod q765;
-pub mod q797;
-pub mod q886;
-pub mod q997;
