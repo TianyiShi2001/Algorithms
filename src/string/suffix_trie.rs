63aa641e87e3dfd758381b5e8d17c7069ed77690
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-06-24 00:44:26 +0100

    occurence of substring

--- src/string/suffix_trie.rs
+++ src/string/suffix_trie.rs
@@ -10,7 +10,7 @@ pub struct NaiveSuffixTrieNode {
 }
 
 impl NaiveSuffixTrie {
-    fn from_str_naive(s: &[u8]) -> Self {
+    pub fn from_str_naive(s: &[u8]) -> Self {
         let n = s.len();
         let mut root = NaiveSuffixTrieNode::default();
         for i in 0..n {
@@ -26,7 +26,8 @@ impl NaiveSuffixTrie {
         Self { root }
     }
 
-    fn contains_substr(&self, query: &[u8]) -> bool {
+    /// Checks whether a substring, `query`, is contained in the string.
+    pub fn contains_substr(&self, query: &[u8]) -> bool {
         let mut node = &self.root;
         for c in query {
             if let Some(child) = node.children.get(c) {
@@ -37,19 +38,59 @@ impl NaiveSuffixTrie {
         }
         true
     }
+
+    /// Count the occurence of the substring, `query`.
+    pub fn occurence(&self, query: &[u8]) -> usize {
+        // this part is essentially the same as `contains_substr`.
+        // if we fail to reconstruct the query by going down the trie,
+        // the string does not contain `query` i.e. we return 0
+        let mut node = &self.root;
+        for c in query {
+            if let Some(child) = node.children.get(c) {
+                node = child;
+            } else {
+                return 0;
+            }
+        }
+        // if `query` can be reconstructed, all the descendent leaves of
+        // `node` represent difference occurences of the query
+        fn dfs(node: &NaiveSuffixTrieNode, count: &mut usize) {
+            for (c, child) in &node.children {
+                if *c == b'$' {
+                    *count += 1;
+                } else {
+                    dfs(child, count);
+                }
+            }
+        }
+        let mut count = 0;
+        dfs(node, &mut count);
+        count
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use lazy_static::lazy_static;
+    lazy_static! {
+        static ref S1: &'static [u8] = b"abracadabra";
+        static ref ST1: NaiveSuffixTrie = NaiveSuffixTrie::from_str_naive(&S1);
+    }
     #[test]
-    fn test_naive_suffix_trie() {
-        let s = b"abracadabra";
-        let st = NaiveSuffixTrie::from_str_naive(s);
-        assert!(st.contains_substr(b"abra"));
-        assert!(st.contains_substr(b"brac"));
-        assert!(st.contains_substr(b"abra"));
-        assert!(!st.contains_substr(b"abrc"));
-        assert!(!st.contains_substr(b"arac"));
+    fn contains_substr_1() {
+        assert!(ST1.contains_substr(b"abra"));
+        assert!(ST1.contains_substr(b"brac"));
+        assert!(ST1.contains_substr(b"abra"));
+        assert!(!ST1.contains_substr(b"abrc"));
+        assert!(!ST1.contains_substr(b"arac"));
+    }
+
+    #[test]
+    fn occurence_1() {
+        assert_eq!(ST1.occurence(b"af"), 0);
+        assert_eq!(ST1.occurence(b"abrac"), 1);
+        assert_eq!(ST1.occurence(b"abra"), 2);
+        assert_eq!(ST1.occurence(b"a"), 5);
     }
 }
