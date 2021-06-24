e4195983d4480a58299e1115a6d1505b85ed7c92
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-06-24 09:24:23 +0100

    longest_repeated_substr

--- src/string/suffix_trie.rs
+++ src/string/suffix_trie.rs
@@ -1,3 +1,6 @@
+/// # Resources
+///
+/// - [Ben Langmead's lecture on "Suffix tries and trees" (2013)](https://www.youtube.com/watch?v=hLsrPsFHPcQ)
 use std::collections::HashMap;
 
 pub struct NaiveSuffixTrie {
@@ -37,9 +40,11 @@ impl NaiveSuffixTrie {
             }
         }
         true
+        // to check if the query is a SUFFIX, check if the final `node`
+        // contains a child with `$`
     }
 
-    /// Count the occurence of the substring, `query`.
+    /// Counts the occurence of the substring, `query`.
     pub fn occurence(&self, query: &[u8]) -> usize {
         // this part is essentially the same as `contains_substr`.
         // if we fail to reconstruct the query by going down the trie,
@@ -67,6 +72,46 @@ impl NaiveSuffixTrie {
         dfs(node, &mut count);
         count
     }
+
+    /// Finds (one of) the longest substring that repeats at least n times.
+    /// Essentially, this means to find the deepest node with at least `n` children.
+    pub fn longest_repeated_substr(&self, n: usize) -> Vec<u8> {
+        fn dfs(
+            node: &NaiveSuffixTrieNode,
+            buffer: &mut Vec<u8>,
+            longest: &mut Vec<u8>,
+            longest_len: &mut usize,
+            n: usize,
+        ) -> usize {
+            let mut descendents_leaves = 0;
+            for (c, child) in &node.children {
+                if *c == b'$' {
+                    // finds a leave
+                    descendents_leaves += 1;
+                } else {
+                    // else add up descendent leaves of all branches
+                    buffer.push(*c);
+                    descendents_leaves += dfs(child, buffer, longest, longest_len, n);
+                    buffer.pop().unwrap();
+                }
+            }
+            if descendents_leaves >= n && buffer.len() > *longest_len {
+                *longest = buffer.clone();
+                *longest_len = buffer.len();
+            }
+            // unsafe {
+            //     println!(
+            //         "{} has {} descendents",
+            //         std::str::from_utf8_unchecked(buffer),
+            //         descendents_leaves
+            //     );
+            // }
+            descendents_leaves
+        }
+        let mut longest = Vec::new();
+        dfs(&self.root, &mut Vec::new(), &mut longest, &mut 0, n);
+        longest
+    }
 }
 
 #[cfg(test)]
@@ -92,5 +137,13 @@ mod tests {
         assert_eq!(ST1.occurence(b"abrac"), 1);
         assert_eq!(ST1.occurence(b"abra"), 2);
         assert_eq!(ST1.occurence(b"a"), 5);
+    }
+
+    #[test]
+    fn longest_repeated_substr_1() {
+        assert_eq!(ST1.longest_repeated_substr(2), b"abra".to_vec());
+        assert_eq!(ST1.longest_repeated_substr(3), vec![b'a']);
+        assert_eq!(ST1.longest_repeated_substr(5), vec![b'a']);
+        assert_eq!(ST1.longest_repeated_substr(6), vec![]);
     }
 }
